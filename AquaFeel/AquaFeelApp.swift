fa8918de35ab7d480aea518269e26ca0be326493
Yanny Nuñez <yannyesteban@hotmail.com>
2024-01-14 09:41:35 -0400

    Initial Commit

---
+++ AquaFeel/AquaFeelApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaFeelApp.swift
+//  AquaFeel
+//
+//  Created by Yanny Esteban on 14/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AquaFeelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AquaFeel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AquaFeel
+//
+//  Created by Yanny Esteban on 14/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
