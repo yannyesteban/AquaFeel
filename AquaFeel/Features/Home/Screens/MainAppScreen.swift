59fbcace8e0bb9140ac17aa4c48e76ec4e83cc22
Yanny Nuñez <yannyesteban@hotmail.com>
2024-02-14 19:01:00 -0400

    First Test

---
+++ AquaFeel/Features/Home/Screens/MainAppScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainAppScreen.swift
+//  AquaFeel
+//
+//  Created by Yanny Esteban on 14/2/24.
+//
+
+import SwiftUI
+
+struct MainAppScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainAppScreen()
+}
---
+++ AquaFeel/Features/User/Models/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  AquaFeel
+//
+//  Created by Yanny Esteban on 14/2/24.
+//
+
+import Foundation
