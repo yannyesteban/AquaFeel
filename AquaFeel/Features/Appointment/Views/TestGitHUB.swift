ad58925e3dcfec47e6db430ca0d4076d936b86b0
Yanny Nuñez <yannyesteban@hotmail.com>
2024-03-14 20:43:17 -0400

    github

---
+++ AquaFeel/Features/Appointment/Views/TestGitHUB.swift
@@ -0,0 +1,18 @@
+//
+//  TestGitHUB.swift
+//  AquaFeel
+//
+//  Created by Yanny Esteban on 14/3/24.
+//
+
+import SwiftUI
+
+struct TestGitHUB: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestGitHUB()
+}
